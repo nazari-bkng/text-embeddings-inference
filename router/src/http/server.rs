b9da17386c85bdd365141081c146caae2da4adf4
Mohammad Nazari <mohammad.nazari@booking.com>
2024-12-24 00:09:07 +0100

    imports

--- router/src/http/server.rs
+++ router/src/http/server.rs
@@ -118,7 +118,7 @@ where
 
     if response_content_type == "application/json" || response_content_type == "application/jsonlines" {
         let (parts, body) = response.into_parts();
-        let bytes = hyper::body::to_bytes(body).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        let bytes = body::to_bytes(body).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
         let response_content = str::from_utf8(&bytes).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
         let predictions: Vec<Value> = serde_json::from_str(response_content)
